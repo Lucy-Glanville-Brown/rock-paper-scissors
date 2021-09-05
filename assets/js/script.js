0031edd33344aebe82b164c5d848e1ce6ca7f970
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2021-09-05 10:41:06 +0000

    Add function to generate computer choice

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,3 +16,20 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
    getResult()
 }))
 
+/**
+ * Generate the computer choice
+ */
+ function generateComputerChoice() {
+   const randomNumber = Math.floor(Math.random() * 3)
+   console.log(randomNumber) //remove once tested
+   if (randomNumber === 0) {
+      computerChoice = 'rock'
+   }
+   if (randomNumber === 1) {
+      computerChoice = 'paper'
+   }
+   if (randomNumber === 2) {
+      computerChoice = 'scissors'
+   }
+   computerChoiceDisplay.innerHTML = computerChoice
+}
