796d0b14f7cae59e797a3937ad544dd7b524785a
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2021-09-05 10:33:05 +0000

    Add event listeners to all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,18 @@
-function runGame() {
+const computerChoiceDisplay = document.getElementById('computer-choice')
+const userChoiceDisplay = document.getElementById('user-choice')
+const resultDisplay = document.getElementById('result')
+const possibleChoices = document.querySelectorAll('button')
+let userChoice
+let computerChoice
+let result
 
-}
+/**
+ * Add event listener for all buttons
+ */
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
+   userChoice = event.target.id
+   userChoiceDisplay.innerHTML = userChoice
+   generateComputerChoice()
+   getResult()
+}))
 
-function checkResult() {
-
-}
-
-function playerWin() {
-
-}
-
-function playerLose() {
-
-}
-
-function playerDraw() {
-
-}
-
-function incrementScore() {
-
-}
