1269bae1f81c8945ce4a8616a4a7ba8d8a752cd5
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2021-09-08 16:51:38 +0000

    Adjustments made from recommendations on JS Hint, missing ;

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,42 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('.control')
-const userImage = document.getElementById('player-image')
-const computerImage = document.getElementById('computer-image')
-let userChoice
-let computerChoice
-let result
-const choices = ['rock', 'paper', 'scissors']
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('.control');
+const userImage = document.getElementById('player-image');
+const computerImage = document.getElementById('computer-image');
+let userChoice;
+let computerChoice;
+let result;
+
 
 /**
  * Add event listener for all control buttons
  */
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (event) => {
-   userChoice = event.target.id
-   userChoiceDisplay.innerHTML = userChoice
-   generateComputerChoice()
-   getResult()
-   userImageDisplay()
-   computerImageDisplay()
-}))
+   userChoice = event.target.id;
+   userChoiceDisplay.innerHTML = userChoice;
+   generateComputerChoice();
+   getResult();
+   userImageDisplay();
+   computerImageDisplay();
+}));
 
 /**
  * Generate the computer choice
  */
  function generateComputerChoice() {
-   const randomNumber = Math.floor(Math.random() * 3)
-   console.log(randomNumber) //remove once tested
+   const randomNumber = Math.floor(Math.random() * 3);
+   console.log(randomNumber); //remove once tested
    if (randomNumber === 0) {
-      computerChoice = 'rock'
+      computerChoice = 'rock';
    }
    if (randomNumber === 1) {
-      computerChoice = 'paper'
+      computerChoice = 'paper';
    }
    if (randomNumber === 2) {
-      computerChoice = 'scissors'
+      computerChoice = 'scissors';
    }
-   computerChoiceDisplay.innerHTML = computerChoice
+   computerChoiceDisplay.innerHTML = computerChoice;
 }
 
 /**
@@ -45,31 +45,31 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 
 function getResult() {
    if (computerChoice === userChoice) {
-      result = "It's a draw!"
+      result = "It's a draw!";
    }
    if (computerChoice === 'rock' && userChoice === 'paper') {
-      result = "You win!"
-      stopTimer()
+      result = "You win!";
+      stopTimer();
    }
    if (computerChoice === 'rock' && userChoice === 'scissors') {
-      result = "You lost!"
+      result = "You lost!";
    }
    if (computerChoice === 'paper' && userChoice === 'scissors') {
-      result = "You win!"
-      stopTimer()
+      result = "You win!";
+      stopTimer();
    }
    if (computerChoice === 'paper' && userChoice === 'rock') {
-      result = "You lost!"
+      result = "You lost!";
    }
    if (computerChoice === 'scissors' && userChoice === 'rock') {
-      result = "You win!"
-      stopTimer()
+      result = "You win!";
+      stopTimer();
    }
    if (computerChoice === 'scissors' && userChoice === 'paper') {
-      result = "You lost!"
+      result = "You lost!";
    }
-   resultDisplay.innerHTML = result
-   incrementScore()
+   resultDisplay.innerHTML = result;
+   incrementScore();
 }
 
 /**
@@ -129,25 +129,25 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
   modal.style.display = "block";
-}
+};
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+};
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
 
 /**
  * Timer to start/stop when user clicks on button
  */
 
-const timer = document.getElementById('timer')
+const timer = document.getElementById('timer');
 let timerInterval;
 
 function startTimer() {
@@ -172,11 +172,12 @@ function startTimer() {
            minute = 0;
         }
    }, 1000);
-};
+}
 
 function stopTimer() {
    clearInterval(timerInterval);
 }
+
 
 
 
